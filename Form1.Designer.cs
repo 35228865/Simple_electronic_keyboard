753b02c706d860417898e1a0a52f7838d1161776
ink99 <ink99@MSI>
2026-05-10 23:37:14 +0800

    加入專案檔案。

---
+++ Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Simple_electronic_keyboard
+{
+    partial class frmBeepPlayer
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.palMain = new System.Windows.Forms.Panel();
+            this.btnHDo = new System.Windows.Forms.Button();
+            this.btnSi = new System.Windows.Forms.Button();
+            this.btnLa = new System.Windows.Forms.Button();
+            this.btnSol = new System.Windows.Forms.Button();
+            this.btnFa = new System.Windows.Forms.Button();
+            this.btnMi = new System.Windows.Forms.Button();
+            this.btnRe = new System.Windows.Forms.Button();
+            this.btnDo = new System.Windows.Forms.Button();
+            this.palMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // palMain
+            // 
+            this.palMain.BackColor = System.Drawing.Color.DimGray;
+            this.palMain.Controls.Add(this.btnHDo);
+            this.palMain.Controls.Add(this.btnSi);
+            this.palMain.Controls.Add(this.btnLa);
+            this.palMain.Controls.Add(this.btnSol);
+            this.palMain.Controls.Add(this.btnFa);
+            this.palMain.Controls.Add(this.btnMi);
+            this.palMain.Controls.Add(this.btnRe);
+            this.palMain.Controls.Add(this.btnDo);
+            this.palMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.palMain.Location = new System.Drawing.Point(0, 0);
+            this.palMain.Name = "palMain";
+            this.palMain.Size = new System.Drawing.Size(617, 300);
+            this.palMain.TabIndex = 0;
+            // 
+            // btnHDo
+            // 
+            this.btnHDo.BackColor = System.Drawing.Color.White;
+            this.btnHDo.FlatAppearance.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnHDo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHDo.Font = new System.Drawing.Font("微軟正黑體", 14F);
+            this.btnHDo.ForeColor = System.Drawing.Color.Red;
+            this.btnHDo.Location = new System.Drawing.Point(533, 12);
+            this.btnHDo.Name = "btnHDo";
+            this.btnHDo.Size = new System.Drawing.Size(75, 285);
+            this.btnHDo.TabIndex = 7;
+            this.btnHDo.Text = "Do\r\n(L)";
+            this.btnHDo.UseVisualStyleBackColor = false;
+            // 
+            // btnSi
+            // 
+            this.btnSi.BackColor = System.Drawing.Color.White;
+            this.btnSi.FlatAppearance.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnSi.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSi.Font = new System.Drawing.Font("微軟正黑體", 14F);
+            this.btnSi.Location = new System.Drawing.Point(458, 12);
+            this.btnSi.Name = "btnSi";
+            this.btnSi.Size = new System.Drawing.Size(75, 285);
+            this.btnSi.TabIndex = 6;
+            this.btnSi.Text = "Si\r\n(J)";
+            this.btnSi.UseVisualStyleBackColor = false;
+            // 
+            // btnLa
+            // 
+            this.btnLa.BackColor = System.Drawing.Color.White;
+            this.btnLa.FlatAppearance.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnLa.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLa.Font = new System.Drawing.Font("微軟正黑體", 14F);
+            this.btnLa.Location = new System.Drawing.Point(383, 12);
+            this.btnLa.Name = "btnLa";
+            this.btnLa.Size = new System.Drawing.Size(75, 285);
+            this.btnLa.TabIndex = 5;
+            this.btnLa.Text = "La\r\n(H)";
+            this.btnLa.UseVisualStyleBackColor = false;
+            // 
+            // btnSol
+            // 
+            this.btnSol.BackColor = System.Drawing.Color.White;
+            this.btnSol.FlatAppearance.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnSol.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSol.Font = new System.Drawing.Font("微軟正黑體", 14F);
+            this.btnSol.Location = new System.Drawing.Point(308, 12);
+            this.btnSol.Name = "btnSol";
+            this.btnSol.Size = new System.Drawing.Size(75, 285);
+            this.btnSol.TabIndex = 4;
+            this.btnSol.Text = "Sol\r\n(G)";
+            this.btnSol.UseVisualStyleBackColor = false;
+            // 
+            // btnFa
+            // 
+            this.btnFa.BackColor = System.Drawing.Color.White;
+            this.btnFa.FlatAppearance.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnFa.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFa.Font = new System.Drawing.Font("微軟正黑體", 14F);
+            this.btnFa.Location = new System.Drawing.Point(233, 12);
+            this.btnFa.Name = "btnFa";
+            this.btnFa.Size = new System.Drawing.Size(75, 285);
+            this.btnFa.TabIndex = 3;
+            this.btnFa.Text = "Fa\r\n(F)";
+            this.btnFa.UseVisualStyleBackColor = false;
+            // 
+            // btnMi
+            // 
+            this.btnMi.BackColor = System.Drawing.Color.White;
+            this.btnMi.FlatAppearance.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnMi.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMi.Font = new System.Drawing.Font("微軟正黑體", 14F);
+            this.btnMi.Location = new System.Drawing.Point(158, 12);
+            this.btnMi.Name = "btnMi";
+            this.btnMi.Size = new System.Drawing.Size(75, 285);
+            this.btnMi.TabIndex = 2;
+            this.btnMi.Text = "Mi\r\n(D)";
+            this.btnMi.UseVisualStyleBackColor = false;
+            // 
+            // btnRe
+            // 
+            this.btnRe.BackColor = System.Drawing.Color.White;
+            this.btnRe.FlatAppearance.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnRe.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRe.Font = new System.Drawing.Font("微軟正黑體", 14F);
+            this.btnRe.Location = new System.Drawing.Point(83, 12);
+            this.btnRe.Name = "btnRe";
+            this.btnRe.Size = new System.Drawing.Size(75, 285);
+            this.btnRe.TabIndex = 1;
+            this.btnRe.Text = "Re\r\n(S)\r\n";
+            this.btnRe.UseVisualStyleBackColor = false;
+            // 
+            // btnDo
+            // 
+            this.btnDo.BackColor = System.Drawing.Color.White;
+            this.btnDo.FlatAppearance.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnDo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDo.Font = new System.Drawing.Font("微軟正黑體", 14F);
+            this.btnDo.Location = new System.Drawing.Point(8, 12);
+            this.btnDo.Name = "btnDo";
+            this.btnDo.Size = new System.Drawing.Size(75, 285);
+            this.btnDo.TabIndex = 0;
+            this.btnDo.Text = "Do\r\n(A)\r\n";
+            this.btnDo.UseVisualStyleBackColor = false;
+            this.btnDo.Click += new System.EventHandler(this.btnDo_Click);
+            // 
+            // frmBeepPlayer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 300);
+            this.Controls.Add(this.palMain);
+            this.KeyPreview = true;
+            this.Name = "frmBeepPlayer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "簡易電子琴";
+            this.Load += new System.EventHandler(this.frmBeepPlayer_Load);
+            this.SizeChanged += new System.EventHandler(this.frmBeepPlayer_SizeChanged);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.frmBeepPlayer_KeyUp);
+            this.palMain.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel palMain;
+        private System.Windows.Forms.Button btnDo;
+        private System.Windows.Forms.Button btnHDo;
+        private System.Windows.Forms.Button btnSi;
+        private System.Windows.Forms.Button btnLa;
+        private System.Windows.Forms.Button btnSol;
+        private System.Windows.Forms.Button btnFa;
+        private System.Windows.Forms.Button btnMi;
+        private System.Windows.Forms.Button btnRe;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Simple_electronic_keyboard
+{
+    public partial class frmBeepPlayer : Form
+    {
+        [DllImport("winmm.dll")]
+        private static extern int midiOutOpen(out IntPtr handle, int deviceID, IntPtr proc, IntPtr instance, int flags);
+
+        [DllImport("winmm.dll")]
+        private static extern int midiOutClose(IntPtr handle);
+
+        [DllImport("winmm.dll")]
+        private static extern int midiOutShortMsg(IntPtr handle, int message);
+
+        private IntPtr midiHandle;
+
+        int[] midiNotes = { 72, 74, 76, 77, 79, 81, 83, 84 };
+
+        int initWidth = 0;
+        int initHeight = 0;
+        Dictionary<string, Rectangle> initControl = new Dictionary<string, Rectangle>();
+
+        // 【新增】一個陣列來儲存按鈕，方便我們用程式控制顏色
+        private Button[] pianoKeys;
+
+        public frmBeepPlayer()
+        {
+            InitializeComponent();
+            InitializeButton();
+
+            // 【新增】初始化按鈕陣列 (順序必須對應 midiNotes 的音高順序)
+            pianoKeys = new Button[] { btnDo, btnRe, btnMi, btnFa, btnSol, btnLa, btnSi, btnHDo };
+
+            // 開啟系統預設的 MIDI 合成器 (-1 代表預設設備)
+            midiOutOpen(out midiHandle, -1, IntPtr.Zero, IntPtr.Zero, 0);
+
+            // 當表單關閉時，記得釋放 MIDI 資源
+            this.FormClosing += FrmBeepPlayer_FormClosing;
+
+            this.KeyPreview = true;
+            this.KeyDown += frmBeepPlayer_KeyDown;
+
+            // 注意：因為你在設計工具已經綁定過 KeyUp 了，這裡就不需要再寫 this.KeyUp +=... 囉！
+        }
+
+        private void InitializeButton()
+        {
+            btnDo.Click += btnDo_Click;
+            btnRe.Click += btnDo_Click;
+            btnMi.Click += btnDo_Click;
+            btnFa.Click += btnDo_Click;
+            btnSol.Click += btnDo_Click;
+            btnLa.Click += btnDo_Click;
+            btnSi.Click += btnDo_Click;
+            btnHDo.Click += btnDo_Click;
+        }
+
+        private void btnDo_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            if (btn == null) return;
+
+            if (int.TryParse(btn.Tag?.ToString(), out int noteIndex) && noteIndex < midiNotes.Length)
+            {
+                PlayNote(noteIndex);
+            }
+            else
+            {
+                PlayNote(btn.TabIndex % midiNotes.Length);
+            }
+        }
+
+        // 【修改】加入按鍵變灰色的視覺回饋
+        private void frmBeepPlayer_KeyDown(object sender, KeyEventArgs e)
+        {
+            int noteIndex = GetNoteIndexFromKey(e.KeyCode);
+
+            if (noteIndex != -1)
+            {
+                PlayNote(noteIndex);
+
+                // 視覺回饋：將按下的琴鍵背景變成淺灰色
+                pianoKeys[noteIndex].BackColor = Color.LightGray;
+
+                e.Handled = true;
+            }
+        }
+
+        // 【新增】這就是編譯器之前找不到的那個方法！手放開時把按鈕變回白色
+        private void frmBeepPlayer_KeyUp(object sender, KeyEventArgs e)
+        {
+            int noteIndex = GetNoteIndexFromKey(e.KeyCode);
+
+            if (noteIndex != -1)
+            {
+                // 視覺回饋：將放開的琴鍵背景恢復成白色
+                pianoKeys[noteIndex].BackColor = Color.White;
+            }
+        }
+
+        // 【新增】將判斷按鍵的邏輯獨立出來，這樣 KeyDown 和 KeyUp 都可以共用
+        private int GetNoteIndexFromKey(Keys key)
+        {
+            switch (key)
+            {
+                case Keys.A: return 0; // Do
+                case Keys.S: return 1; // Re
+                case Keys.D: return 2; // Mi
+                case Keys.F: return 3; // Fa
+                case Keys.G: return 4; // Sol
+                case Keys.H: return 5; // La
+                case Keys.J: return 6; // Si
+                case Keys.K: return 7; // High Do
+                default: return -1;
+            }
+        }
+
+        private void PlayNote(int index)
+        {
+            if (index >= 0 && index < midiNotes.Length)
+            {
+                int note = midiNotes[index];
+                int msg = 0x90 | (note << 8) | (127 << 16);
+                midiOutShortMsg(midiHandle, msg);
+            }
+        }
+
+        private void FrmBeepPlayer_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (midiHandle != IntPtr.Zero)
+            {
+                midiOutClose(midiHandle);
+            }
+        }
+
+        private void frmBeepPlayer_Load(object sender, EventArgs e)
+        {
+            this.initWidth = this.palMain.Width;
+            this.initHeight = this.palMain.Height;
+
+            foreach (Control ctl in this.palMain.Controls)
+            {
+                this.initControl.Add(ctl.Name, new Rectangle(ctl.Left, ctl.Top, ctl.Width, ctl.Height));
+            }
+        }
+
+        private void frmBeepPlayer_SizeChanged(object sender, EventArgs e)
+        {
+            if (this.initWidth == 0 || this.initHeight == 0) return;
+
+            double width = this.palMain.Width;
+            double height = this.palMain.Height;
+            double iRatioWith = width / this.initWidth;
+            double iRatioHeight = height / this.initHeight;
+
+            foreach (Control ctl in this.palMain.Controls)
+            {
+                if (initControl.ContainsKey(ctl.Name))
+                {
+                    Rectangle originalRect = initControl[ctl.Name];
+                    ctl.Left = (int)(originalRect.Left * iRatioWith);
+                    ctl.Top = (int)(originalRect.Top * iRatioHeight);
+                    ctl.Width = (int)(originalRect.Width * iRatioWith);
+                    ctl.Height = (int)(originalRect.Height * iRatioHeight);
+                }
+            }
+        }
+    }
+}
